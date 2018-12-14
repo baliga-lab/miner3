c1d528ca56e3ae52bf5fa219203b757f08877204
MattWallScientist <45861672+MattWallScientist@users.noreply.github.com>
2018-12-13 23:25:14 -0800

    Add files via upload

---
+++ miner/src/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 12 13:06:43 2018
+
+@author: MattWall
+"""
+
+from __future__ import absolute_import
+from .miner import miner
+from .miner2 import miner2
